68f4716c568e89f330637e6307655c306062a8fb
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-21 19:51:17 +0200

    Store data in the local database

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,21 +1,27 @@
 import * as FileSystem from 'expo-file-system'
+import { insertPlace } from '../../helpers/db'
 
 export const ADD_PLACE = 'ADD_PLACE'
 
 export const addPlace = (title, imageUri) => async (dispatch) => {
-  const fileName = image.split('/').pop()
+  const fileName = imageUri.split('/').pop()
 
   const newPath = FileSystem.documentDirectory + fileName
 
   try {
     await FileSystem.moveAsync({
-      from: image,
+      from: imageUri,
       to: newPath,
     })
+    const dbRes = await insertPlace(title, newPath, 'Dummy address', 15.6, 12.3)
+    console.log(dbRes)
   } catch (err) {
     console.log(err)
     throw err
   }
 
-  dispatch({ type: ADD_PLACE, payload: { title, imageUri: newPath } })
+  dispatch({
+    type: ADD_PLACE,
+    payload: { id: dbRes.insertId, title, imageUri: newPath },
+  })
 }
