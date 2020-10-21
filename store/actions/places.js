6ae0ad8be83d641874de04b47296e00138099022
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-21 19:18:10 +0200

    Store the image on the FileSystem

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,5 +1,21 @@
+import * as FileSystem from 'expo-file-system'
+
 export const ADD_PLACE = 'ADD_PLACE'
 
-export const addPlace = (title, imageUri) => {
-  return { type: ADD_PLACE, payload: { title, imageUri } }
+export const addPlace = (title, imageUri) => async (dispatch) => {
+  const fileName = image.split('/').pop()
+
+  const newPath = FileSystem.documentDirectory + fileName
+
+  try {
+    await FileSystem.moveAsync({
+      from: image,
+      to: newPath,
+    })
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+
+  dispatch({ type: ADD_PLACE, payload: { title, imageUri: newPath } })
 }
