c593a7ed49fd0aadf220207646304881cdde3dcb
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-21 19:32:33 +0200

    Initial SQLite database

--- App.js
+++ App.js
@@ -2,8 +2,18 @@ import React from 'react'
 import { Provider } from 'react-redux'
 
 import store from './store/index'
+import { init } from './helpers/db'
 
 import PlacesNavigator from './navigation/PlacesNavigator'
+
+init()
+  .then(() => {
+    console.log('Initialized database')
+  })
+  .catch((err) => {
+    console.log('Initialized failed')
+    console.log(err)
+  })
 
 export default function App() {
   return (
