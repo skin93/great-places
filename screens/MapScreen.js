c539b27dd70418dc81324e9f1118e4300f52fe6b
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-22 13:24:53 +0200

    Add a marker

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 
 const MapScreen = (props) => {
+  const [selectedLocation, setSelectedLocation] = useState()
   const mapRegion = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   }
-  return <MapView style={styles.map} region={mapRegion} />
+
+  const selectLocationHandler = (event) => {
+    setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lng: event.nativeEvent.coordinate.longitude,
+    })
+  }
+
+  let markerCoordinates
+
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.lat,
+      longitude: selectedLocation.lng,
+    }
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      region={mapRegion}
+      onPress={selectLocationHandler}
+    >
+      {markerCoordinates && (
+        <Marker title='Picked Location' coordinate={markerCoordinates}></Marker>
+      )}
+    </MapView>
+  )
 }
 
 export default MapScreen
